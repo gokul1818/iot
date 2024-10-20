207d3a76b3df9a370ca916913e5b6420cadb6111
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-10-20 20:22:06 +0530

    updateddd

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,6 +1,6 @@
 // src/components/Clock.js
 import React, { useEffect, useState } from 'react';
-
+import "./clock.css"
 const Clock = () => {
     const [time, setTime] = useState(new Date());
 
@@ -21,10 +21,39 @@ const Clock = () => {
         }
     };
 
+    const formatTime = () => {
+        let hours = time.getHours();
+        const minutes = time.getMinutes().toString().padStart(2, '0'); // Get minutes and pad with zero if needed
+        const ampm = hours >= 12 ? 'PM' : 'AM'; // Determine AM/PM
+
+        // Convert to 12-hour format
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+
+        return { hours: hours.toString().padStart(2, '0'), minutes };
+    };
+
+    const { hours, minutes } = formatTime();
+
+    const showtime = () => {
+        return (
+
+            <div className='format-time-style1  '>
+                <p className='time-show1'>{hours[0]}</p>
+                <p className='time-show2'>{hours[1]}</p>
+                <p className='time-show3'>{minutes[0]}</p>
+                <p className='time-show4'>{minutes[1]}</p>
+            </div>
+        )
+    }
+
+
     return (
         <div className='d-flex align-items-center justify-content-between'>
-            <h2 >{getGreeting()}</h2>
-            <h6>{time.toLocaleTimeString()}</h6>
+            <h2>{getGreeting()}</h2>
+            <div>  {showtime()}
+                {/* <p>{time.getHours() >= 12 ? 'PM' : 'AM'}</p> Display AM/PM */}
+            </div>
         </div>
     );
 };
