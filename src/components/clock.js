60f1824db9dd38371455f9b448e3b6322747353a
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-10-20 15:28:33 +0530

    update

---
+++ src/components/clock.js
@@ -0,0 +1,32 @@
+// src/components/Clock.js
+import React, { useEffect, useState } from 'react';
+
+const Clock = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const timerId = setInterval(() => setTime(new Date()), 1000);
+        return () => clearInterval(timerId);
+    }, []);
+
+    const getGreeting = () => {
+        const hours = time.getHours();
+
+        if (hours < 12) {
+            return "Good Morning";
+        } else if (hours < 18) {
+            return "Good Afternoon";
+        } else {
+            return "Good Night";
+        }
+    };
+
+    return (
+        <div className='d-flex align-items-center justify-content-between'>
+            <h2 >{getGreeting()}</h2>
+            <h3>{time.toLocaleTimeString()}</h3>
+        </div>
+    );
+};
+
+export default Clock;
